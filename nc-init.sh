583b43425e61c68b1fb8fb34c5aae958cf64fdac
sky321 <github.10.csw@spamgourmet.com>
2019-05-25 17:56:09 +0200

    toggle 4byte to test

--- nc-init.sh
+++ nc-init.sh
@@ -55,8 +55,8 @@ configure()
   mysql <<EOF
 DROP DATABASE IF EXISTS nextcloud;
 CREATE DATABASE nextcloud;
-#    CHARACTER SET utf8mb4
-#    COLLATE utf8mb4_general_ci;
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_general_ci;
 GRANT USAGE ON *.* TO '$DBADMIN'@'localhost' IDENTIFIED BY '$DBPASSWD';
 DROP USER '$DBADMIN'@'localhost';
 CREATE USER '$DBADMIN'@'localhost' IDENTIFIED BY '$DBPASSWD';
@@ -115,7 +115,7 @@ EOF
   sed -i "s|^;\?sys_temp_dir =.*$|sys_temp_dir = $UPLOADTMPDIR|"     /etc/php/${PHPVER}/fpm/php.ini
 
   # 4 Byte UTF8 support
-  #sudo -u www-data php occ config:system:set mysql.utf8mb4 --type boolean --value="true"
+  sudo -u www-data php occ config:system:set mysql.utf8mb4 --type boolean --value="true"
   
   IFACE="$( ip r | grep "default via" | awk '{ print $5 }' | head -1 )"
   IP="$( ip a show dev "$IFACE" | grep global | grep -oP '\d{1,3}(.\d{1,3}){3}' | head -1 )" 

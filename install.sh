6b91b2684c82a6f83a3cc2504ef5eb1ed4e57324
sky321 <github.10.csw@spamgourmet.com>
2019-07-06 17:29:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ DISTRO_2=$( grep DISTRO_2 /root/.nextpi.cnf | sed 's|DISTRO_2=||' )
   exit 1
 }
 
-# check_distro 
+# check_distro   
 grep -q -e $DISTRO_1 -e $DISTRO_2 /etc/issue || {
   echo "distro not supported"; 
   exit 1; 

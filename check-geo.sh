5d1c568b185636384fb5e4e49dccf840879c3797
skyPad <ab@cd.de>
2019-10-21 07:39:08 +0200

    exclude some fields

--- check-geo.sh
+++ check-geo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 IP=$1
 APIKEY=xxxxxxxxxxxxx
-curl "https://api.ipgeolocation.io/ipgeo?apiKey=${APIKEY}&ip=${IP}&fields=geo&output=xml"
+curl "https://api.ipgeolocation.io/ipgeo?apiKey=${APIKEY}&ip=${IP}&fields=geo&excludes=country_code3,country_code2,zipcode&output=xml"

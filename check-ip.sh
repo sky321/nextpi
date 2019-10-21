02e8de6b1534cbe1baa3c10bb201e693b2ea1676
skyPad <ab@cd.de>
2019-10-21 07:28:54 +0200

    clear check scripts

--- check-ip.sh
+++ check-ip.sh
@@ -2,14 +2,9 @@
 IP=$1
 logfile='/var/log/apache2/access.log.1'
 
-#echo "----------------------Request"
-#sudo awk -F\" '{print $2}' ${logfile} | sort | uniq -c | sort
-
 echo "----------------------What did a special IP?"
 sudo awk '($1 ~ /'$IP'/)' ${logfile} | awk '{print $9,$7}' | sort | uniq -c
 echo "----------------------"
 sudo awk '($1 ~ /'$IP'/)' ${logfile} | sort | uniq -c
 
-#echo "-------------------Show request from selected status code"
-#sudo awk '($9 ~ 207)' /var/log/apache2/access.log.1 | awk -F\" '{print $2}' | sort | uniq -c
 

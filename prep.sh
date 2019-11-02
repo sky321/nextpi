ecd2b00cc06eae601eb1aba3a639be4c7a081e7f
sky321 <github.10.csw@spamgourmet.com>
2019-11-02 11:53:02 +0100

    passwd change

--- prep.sh
+++ prep.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -uxe
+#!/bin/bash
 
 ## 
 #
@@ -7,18 +7,39 @@
 #
 ##
 
+echo
+echo
 echo "----- change standard password for PI -----"
-passwd pi
+echo
+echo
+passwd
 
+echo
+echo
 echo "----- update the OS -----"
+echo
+echo
 sudo apt-get update -y && sudo apt-get upgrade -y
 
+
+echo
+echo
 echo "----- install git -----"
+echo
+echo
 sudo apt-get install -y git
 
+echo
+echo
 echo "----- get repo from git -----"
+echo
+echo
 git clone https://github.com/sky321/nextpi.git
 cd nextpi
 
+echo
+echo
 echo "----- generate new keys -----"
+echo
+echo
 sudo ./genhostkey.sh

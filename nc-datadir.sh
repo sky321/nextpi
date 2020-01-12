65a50c2ab9e3f5551043621bd707aeb014af523d
sky321 <github.10.csw@spamgourmet.com>
2020-01-12 12:49:09 +0100

    remove redundant mkdir

--- nc-datadir.sh
+++ nc-datadir.sh
@@ -30,7 +30,7 @@ BASEDIR=$( dirname "$DATADIR" )
   [[ "$SRCDIR" == "$DATADIR" ]] && { echo -e "INFO: data already there"; exit 1; }
 
 #  [ -d "$BASEDIR" ] || { echo "$BASEDIR does not exist"; exit 1; }
-  mkdir -p $BASEDIR
+#  mkdir -p $BASEDIR
 
 # start
 

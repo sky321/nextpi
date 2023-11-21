5c0c088a0a9917d95ae1eb379ef703ddf825a38b
skyPad <ab@cd.de>
2023-11-21 10:12:06 +0100

    make crowdsec fit to nextpi

--- crowdsec.sh
+++ crowdsec.sh
@@ -18,6 +18,7 @@ then
     echo ""
     exit 1
 fi
+
 echo ""
 echo " » fail2ban wird entfernt  // remove fail2ban"
 echo ""
@@ -25,21 +26,24 @@ sleep 2
 systemctl stop fail2ban.service
 systemctl disable fail2ban.service
 systemctl mask fail2ban.service
-apt remove fail2ban --purge -y
+apt-get remove fail2ban --purge -y
 clear
 echo ""
 echo " » fail2ban entfernt // removed           [OK]"
+
 echo " » Crowdsec wird heruntergeladen+installiert // crowdsec will be downloaded+installed"
 echo ""
 sleep 2
 curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
-apt install crowdsec -y
-apt install crowdsec-firewall-bouncer-nftables -y
+apt-get install crowdsec -y
+apt-get install crowdsec-firewall-bouncer-nftables -y
 systemctl enable --now crowdsec.service
+
 cscli collections install crowdsecurity/nextcloud
-cscli collections install crowdsecurity/nginx
+cscli collections install crowdsecurity/apache2
 cscli collections install crowdsecurity/sshd
 systemctl reload crowdsec && systemctl restart crowdsec
+
 cp /etc/crowdsec/acquis.yaml /etc/crowdsec/acquis.yaml.bak
 cat <<EOF >>/etc/crowdsec/acquis.yaml
 #Nextcloud by c-rieger.de
@@ -49,7 +53,9 @@ labels:
   type: Nextcloud
 ---
 EOF
+
 systemctl reload crowdsec && systemctl restart crowdsec.service crowdsec-firewall-bouncer.service
+
 clear
 echo ""
 echo " » fail2ban entfernt // removed           [OK]"

526fdad9cbd3465b7ae4f97e651cc4353e08f6d1
skyPad <ab@cd.de>
2023-11-21 13:54:59 +0100

    prepare install for crowdsec instead of fail2ban

--- install.sh
+++ install.sh
@@ -50,14 +50,16 @@ source library.sh
 install_script  lamp.sh
 install_script  nc-nextcloud.sh
 install_script  unattended-upgrades.sh
-install_script  fail2ban.sh
+# install_script  fail2ban.sh
 install_script  ufw.sh
+install_script  crowdsec.sh
 activate_script nc-nextcloud.sh
 activate_script nc-init.sh
 activate_script unattended-upgrades.sh
-activate_script fail2ban.sh
+# activate_script fail2ban.sh
 activate_script syscfg.sh
 activate_script ufw.sh
+activate_script crowdsec.sh
 
 
 # post install

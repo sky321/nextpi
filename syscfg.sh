e6823803a06daee0e94f56cd0247d2f061c22b64
skyPad <ab@cd.de>
2023-12-20 16:43:53 +0100

    icmp block commented

--- syscfg.sh
+++ syscfg.sh
@@ -87,6 +87,8 @@ net.ipv4.conf.default.log_martians=1
 net.ipv4.tcp_timestamps=0
 net.ipv4.icmp_echo_ignore_broadcasts = 1
 net.ipv4.tcp_syncookies = 1
+#net.ipv4.icmp_echo_ignore_all=1
+#net.ipv6.icmp.echo_ignore_all=1
 net.ipv6.conf.all.accept_redirects=0
 net.ipv6.conf.default.accept_redirects=0
 EOF

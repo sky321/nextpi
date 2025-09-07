e0a093c3dd6f8c243ef4891a549448e9bbe53808
chris <git.10.csw@spamgourmet.com>
2025-09-07 16:12:06 +0200

    Android test

--- crowdsec.sh
+++ crowdsec.sh
@@ -55,7 +55,7 @@ labels:
 ---
 EOF
 
-# get IP
+# personal whitelist Witz own ip
 IFACE="$( ip r | grep "default via" | awk '{ print $5 }' | head -1 )"
 IP="$( ip a show dev "$IFACE" | grep global | grep -oP '\d{1,3}(.\d{1,3}){3}' | head -1 )"
 

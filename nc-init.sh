ad3650fcff06dd2779e23113e00ad430d4f3f9e9
sky321 <git.10.csw@spamgouret.com>
2026-06-19 11:51:34 +0200

    add nextcloud config std values after install

--- nc-init.sh
+++ nc-init.sh
@@ -167,6 +167,7 @@ EOF
   #disable some apps by default
   sudo -u www-data php /var/www/nextcloud/occ app:disable survey_client
   sudo -u www-data php /var/www/nextcloud/occ app:disable firstrunwizard
+  sudo -u www-data php /var/www/nextcloud/occ app:disable app_api
   
   # other
   sudo -u www-data php /var/www/nextcloud/occ config:system:set overwriteprotocol --value=https
@@ -175,6 +176,8 @@ EOF
   sudo -u www-data php /var/www/nextcloud/occ config:system:set trashbin_retention_obligation --value="auto, 30"
   sudo -u www-data php /var/www/nextcloud/occ config:system:set log_rotate_size --value=10485760 --type=integer
   sudo -u www-data php /var/www/nextcloud/occ config:system:set default_phone_region --value="DE"
+  sudo -u www-data php /var/www/nextcloud/occ config:system:set maintenance_window_start --value=1
+  sudo -u www-data php /var/www/nextcloud/occ config:system:set serverid --value=1 --type=integer
 #  sudo -u www-data php /var/www/nextcloud/occ -n db:convert-filecache-bigint
 
   echo "NC init done

baa285823b6b2fbaa80dc30193d7b5f82b50382c
sky321 <git.10.csw@spamgouret.com>
2026-06-23 12:26:51 +0200

    remove sury repo again

--- lamp.sh
+++ lamp.sh
@@ -34,9 +34,9 @@ install()
     $APTINSTALL curl apt-transport-https ca-certificates
 
     # include Sury Repo 
-    curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
-    dpkg -i /tmp/debsuryorg-archive-keyring.deb
-    sh -c 'echo "deb [signed-by=/usr/share/keyrings/debsuryorg-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+#    curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
+#    dpkg -i /tmp/debsuryorg-archive-keyring.deb
+#    sh -c 'echo "deb [signed-by=/usr/share/keyrings/debsuryorg-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
   
 
     # INSTALL 
